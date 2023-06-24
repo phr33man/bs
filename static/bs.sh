af706085c4a0cd196d467932233e7836dc4dc952
5701c <dennis@rwxstoic.com>
2023-06-24 22:37:31 +0200

    bs

--- static/bs.sh
+++ static/bs.sh
@@ -4,7 +4,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/phr33man/dotfiles.git"
+#dotfilesrepo="https://github.com/phr33man/dotfiles.git"
+dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 progsfile="${parent_path}/progs.csv"
 aurhelper="yay"
